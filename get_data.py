ac16eec749900059a3d3a4a935621b4db7ec07ae
evan-erasmus <evanerasmus14@gmail.com>
2023-10-05 17:07:01 +0200

    added specific file choice

--- get_data.py
+++ get_data.py
@@ -26,7 +26,34 @@ def get_sorted_data():
         for record in set:
             result_data.append(record)
 
-    result_data = list(unique_everseen(result_data))
+    # result_data = list(unique_everseen(result_data))
+    sorted_data = sorted(result_data, key=custom_sort_key)
+
+    return sorted_data
+
+
+def get_specific_filedata(choice):
+    combined_data = []
+    result_data = []
+
+    i = 0
+
+    # for filename in filenames:
+    csv_file_path = f'{data_dir}/{filenames[i]}'
+    data_list = []
+
+    with open(csv_file_path, mode='r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data_list.append(row)
+
+    combined_data.append(data_list)
+
+    for set in combined_data:
+        for record in set:
+            result_data.append(record)
+
+    # result_data = list(unique_everseen(result_data))
     sorted_data = sorted(result_data, key=custom_sort_key)
 
     return sorted_data
